c881f36bf555a9cdd19cdbef887564c16450d2a2
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-08 17:37:55 +0000

    añadidos los atributos de la clase

--- lib/recursos_educativos/recurso.rb
+++ lib/recursos_educativos/recurso.rb
@@ -1,4 +1,6 @@
 module RecursosEducativos
   class Recurso
+    def initialize(id, marca, titulo, descripcion, nivel_experiencia,tipo_actividad, categoria, material, temporalizacion, concepto)
+    end
   end
 end
