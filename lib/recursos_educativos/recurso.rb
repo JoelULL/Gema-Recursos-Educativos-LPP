1dd558d4eda508293a0396c7e9380e470b39668f
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-08 18:11:59 +0000

    añadido el attr reader para los getter

--- lib/recursos_educativos/recurso.rb
+++ lib/recursos_educativos/recurso.rb
@@ -1,6 +1,17 @@
 module RecursosEducativos
   class Recurso
+    attr_reader :id, :marca, :titulo, :descripcion, :nivel_experiencia, :tipo_actividad, :categoria,:material, :temporalizacion, :concepto
     def initialize(id, marca, titulo, descripcion, nivel_experiencia,tipo_actividad, categoria, material, temporalizacion, concepto)
+      @id = id
+      @marca = marca
+      @titulo = titulo
+      @descripcion = descripcion
+      @nivel_experiencia = nivel_experiencia
+      @tipo_actividad = tipo_actividad
+      @categoria = categoria
+      @material = material
+      @temporalizacion = temporalizacion
+      @concepto = concepto
     end
   end
 end
