1b4c383ad299dcc77da4f874b8a16a3dc25f1fb8
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-09 15:39:28 +0000

    Arreglo de las constantes declaradas

--- lib/recursos_educativos/constantes_recurso.rb
+++ lib/recursos_educativos/constantes_recurso.rb
@@ -1,6 +1,6 @@
 #Constantes utilizadas. Se representa el nivel de los recursos
 module RecursosEducativos
-  BEGINNER = "beginner"
-  INTERMEDIATE = "intermediate"
-  EXPERT = "expert"
+  BEGINNER = :beginner
+  INTERMEDIATE = :intermediate
+  EXPERT = :expert
 end
