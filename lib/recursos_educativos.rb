cb0fc2fadb3a9af10c6d0bb589a0ae5e77dc986d
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-09 16:11:39 +0000

    añadidos comentarios que faltaban

--- lib/recursos_educativos.rb
+++ lib/recursos_educativos.rb
@@ -3,10 +3,11 @@
 require_relative "recursos_educativos/version"
 require "recursos_educativos/recurso"
 require "recursos_educativos/constantes_recurso"
-
+#Modulo principal de la gema que representa Recursos educativos.
 module RecursosEducativos
   class Error < StandardError; end
   # Your code goes here...
+#la siguiente funcion recibe un objeto del mismo espacio de nombres y si devuelve el valor del atributo temporalizacion del objeto (recurso).
   def self.obtener_temporalizacion(recurso)
     return recurso.temporalizacion
   end
