f7129fe6123d5fcbdd172a7f48cc85483c89b29c
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-14 20:36:55 +0000

    arreglo de error

--- lib/recursos_educativos/entorno_digital.rb
+++ lib/recursos_educativos/entorno_digital.rb
@@ -1,7 +1,7 @@
 module RecursosEducativos
   class EntornoDigital
     def initialize(id_code,nombre,categoria,coleccion)
-      @id_code = id
+      @id_code = id_code
       @nombre = nombre
       @categoria = categoria
       @coleccion = coleccion
