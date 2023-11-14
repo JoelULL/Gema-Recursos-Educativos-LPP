cacf22ff6f339c6ddbbca44eef5cb2fab4771cf2
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-14 21:00:04 +0000

    añadidas las pruebas para los getters de la clase entornodigital

--- test/entorno_digital_test.rb
+++ test/entorno_digital_test.rb
@@ -4,4 +4,20 @@ class EntornoDigitalTest < Test::Unit::TestCase
     @default = RecursosEducativos::EntornoDigital.new(1,"nombre",:taller,[])
     assert_instance_of(RecursosEducativos::EntornoDigital,@default)
   end
+  def setup
+     @recurso_1 = RecursosEducativos::Recurso.new(1,"marca","titulo","descripcion",RecursosEducativos::BEGINNER,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
+     @recurso_2 = RecursosEducativos::Recurso.new(2,"marca","titulo","descripcion",RecursosEducativos::EXPERT,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
+#INTERMEDIATE
+    @recurso_3 = RecursosEducativos::Recurso.new(3,"marca","titulo","descripcion",RecursosEducativos::INTERMEDIATE,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
+
+    @entorno_default = RecursosEducativos::EntornoDigital.new(1,"nombre",:taller,[@recurso_1,@recurso_2,@recurso_3])
+  end
+
+  def test_getters
+   #assert_equal(1,@recurso_default.id)
+    assert_equal(1,@entorno_default.id_code)
+    assert_equal("nombre",@entorno_default.nombre)
+    assert_equal(:taller,@entorno_default.categoria)
+    assert_equal([@recurso_1,@recurso_2,@recurso_3],@entorno_default.coleccion) 
+  end
 end
