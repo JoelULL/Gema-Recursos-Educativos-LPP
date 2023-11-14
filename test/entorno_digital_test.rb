ae358f10b200c1987f7016f8e61c4c28d892bb06
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-14 21:17:59 +0000

    añadido el test del to_s para entornos digitales

--- test/entorno_digital_test.rb
+++ test/entorno_digital_test.rb
@@ -5,10 +5,10 @@ class EntornoDigitalTest < Test::Unit::TestCase
     assert_instance_of(RecursosEducativos::EntornoDigital,@default)
   end
   def setup
-     @recurso_1 = RecursosEducativos::Recurso.new(1,"marca","titulo","descripcion",RecursosEducativos::BEGINNER,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
-     @recurso_2 = RecursosEducativos::Recurso.new(2,"marca","titulo","descripcion",RecursosEducativos::EXPERT,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
+     @recurso_1 = RecursosEducativos::Recurso.new(1,"marca","titulo1","descripcion",RecursosEducativos::BEGINNER,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
+     @recurso_2 = RecursosEducativos::Recurso.new(2,"marca","titulo2","descripcion",RecursosEducativos::EXPERT,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
 #INTERMEDIATE
-    @recurso_3 = RecursosEducativos::Recurso.new(3,"marca","titulo","descripcion",RecursosEducativos::INTERMEDIATE,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
+    @recurso_3 = RecursosEducativos::Recurso.new(3,"marca","titulo3","descripcion",RecursosEducativos::INTERMEDIATE,"tipo","categoria","material",60,[:razonamiento, :abstraccion, :descomposicion, :patrones, :algoritmos, :codificacion,:validacion])
 
     @entorno_default = RecursosEducativos::EntornoDigital.new(1,"nombre",:taller,[@recurso_1,@recurso_2,@recurso_3])
   end
@@ -19,5 +19,8 @@ class EntornoDigitalTest < Test::Unit::TestCase
     assert_equal("nombre",@entorno_default.nombre)
     assert_equal(:taller,@entorno_default.categoria)
     assert_equal([@recurso_1,@recurso_2,@recurso_3],@entorno_default.coleccion) 
+  end
+  def test_to_s
+    assert_equal("- Id :1 - nombre: nombre -categoria: taller -coleccion:titulo1, titulo2, titulo3",@entorno_default.to_s)
   end
 end
