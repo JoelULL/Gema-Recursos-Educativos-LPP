ffdce5b59a5be5ff9bedc5a61a4e6d4475d8f9ed
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-14 19:24:47 +0000

    añadidas pruebas de herencia entre recurso y digitales abiertos

--- test/digitales_abiertos_test.rb
+++ test/digitales_abiertos_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 class DigitalesAbiertosTest < Test::Unit::TestCase
   def test_class_empty
-    assert_nothing_raised do
-      RecursosEducativos::DigitalesAbiertos.new(1,"marca","titulo","descripcion","nivel","tipo","categoria","material",60,[:razonamiento])
-    end
+    assert_true(RecursosEducativos::DigitalesAbiertos.new(1,"marca","titulo","descripcion","nivel","tipo","categoria","material",60,[:razonamiento]).instance_of?(RecursosEducativos::DigitalesAbiertos))
+  end
+  def test_herencia
+    assert_true(RecursosEducativos::DigitalesAbiertos.new(1,"marca","titulo","descripcion","nivel","tipo","categoria","material",60,[:razonamiento]).is_a?(RecursosEducativos::Recurso))
+    assert_true(RecursosEducativos::DigitalesAbiertos.is_a?(Module))
+    assert_true(RecursosEducativos::DigitalesAbiertos.is_a?(Object))
+    assert_true(RecursosEducativos::DigitalesAbiertos.is_a?(BasicObject))
   end
 end
