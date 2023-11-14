f59c937ec14cd706e3fa0d8ff387b2f6594d6a4b
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-14 19:40:06 +0000

    añadida la prueba del initialize con los nuevos atributos de la clase hija. Funciona.

--- test/digitales_abiertos_test.rb
+++ test/digitales_abiertos_test.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 class DigitalesAbiertosTest < Test::Unit::TestCase
   def test_class_empty
-    assert_true(RecursosEducativos::DigitalesAbiertos.new(1,"marca","titulo","descripcion","nivel","tipo","categoria","material",60,[:razonamiento]).instance_of?(RecursosEducativos::DigitalesAbiertos))
+    assert_true(RecursosEducativos::DigitalesAbiertos.new(1,"marca","titulo","descripcion","nivel","tipo","categoria","material",60,[:razonamiento],"foo://example.com:8042/over/there?name=ferret#nose",2012).instance_of?(RecursosEducativos::DigitalesAbiertos))
   end
   def test_herencia
-    assert_true(RecursosEducativos::DigitalesAbiertos.new(1,"marca","titulo","descripcion","nivel","tipo","categoria","material",60,[:razonamiento]).is_a?(RecursosEducativos::Recurso))
+    assert_true(RecursosEducativos::DigitalesAbiertos.new(1,"marca","titulo","descripcion","nivel","tipo","categoria","material",60,[:razonamiento],"foo://example.com:8042/over/there?name=ferret#nose",2012).is_a?(RecursosEducativos::Recurso))
     assert_true(RecursosEducativos::DigitalesAbiertos.is_a?(Module))
     assert_true(RecursosEducativos::DigitalesAbiertos.is_a?(Object))
     assert_true(RecursosEducativos::DigitalesAbiertos.is_a?(BasicObject))
