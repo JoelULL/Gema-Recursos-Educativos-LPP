f18a5cf01d666e080ddd9cf2c408029d5fadbccd
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-14 19:31:59 +0000

    añadido el initialize a la clase digitales abiertos

--- lib/recursos_educativos/digitales_abiertos.rb
+++ lib/recursos_educativos/digitales_abiertos.rb
@@ -1,4 +1,9 @@
 module RecursosEducativos
   class DigitalesAbiertos < Recurso
+    def initialize(id, marca, titulo, descripcion, nivel_experiencia,tipo_actividad, categoria, material, temporalizacion, concepto, uri, fecha_creacion)
+          super(id, marca, titulo, descripcion, nivel_experiencia,tipo_actividad, categoria, material, temporalizacion, concepto)
+    @uri = uri
+    @fecha_creacion = fecha_creacion
+    end
   end
 end
