143cdc7632d76edd81a124d3b565b3e2f8186d5b
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-14 19:42:47 +0000

    añadidos getters de los atributos propios de la clase hija

--- lib/recursos_educativos/digitales_abiertos.rb
+++ lib/recursos_educativos/digitales_abiertos.rb
@@ -1,5 +1,6 @@
 module RecursosEducativos
   class DigitalesAbiertos < Recurso
+    attr_reader :uri, :fecha_creacion
     def initialize(id, marca, titulo, descripcion, nivel_experiencia,tipo_actividad, categoria, material, temporalizacion, concepto, uri, fecha_creacion)
           super(id, marca, titulo, descripcion, nivel_experiencia,tipo_actividad, categoria, material, temporalizacion, concepto)
     @uri = uri
