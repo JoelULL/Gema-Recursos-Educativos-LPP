e2ff632706edbaf40fa902aa4eb4603660b12048
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-14 20:24:54 +0000

    añadido el initialize para la clase entorno digital

--- lib/recursos_educativos/entorno_digital.rb
+++ lib/recursos_educativos/entorno_digital.rb
@@ -1,4 +1,10 @@
 module RecursosEducativos
   class EntornoDigital
+    def initialize(id_code,nombre,categoria,coleccion)
+      @id_code = id
+      @nombre = nombre
+      @categoria = categoria
+      @coleccion = coleccion
+    end
   end
 end
