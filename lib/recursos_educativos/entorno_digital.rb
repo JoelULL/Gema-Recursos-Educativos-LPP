c5ba6ffb9543592fdad08414aff5ff7487715ab0
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-15 19:05:35 +0000

    añadida funcion para el nivel medio de la coleccion de un entorno digital

--- lib/recursos_educativos/entorno_digital.rb
+++ lib/recursos_educativos/entorno_digital.rb
@@ -14,5 +14,25 @@ module RecursosEducativos
     def numero_de_recursos_coleccion
       @coleccion.size
     end
+    def nivel_medio
+      # Asociar un número a cada nivel de dificultad directamente
+      valores_niveles = {
+        RecursosEducativos::BEGINNER => 1,
+        RecursosEducativos::INTERMEDIATE => 2,
+        RecursosEducativos::EXPERT => 3
+      }
+      valores = (@coleccion.categoria).map { |nivel| valores_niveles[nivel] }
+
+      # Calcula el promedio dividiendo la suma total por la cantidad de elementos
+      promedio = valores.sum.to_f / valores.size
+      case promedio
+      when 0..1
+        RecursosEducativos::BEGINNER
+      when 2
+        RecursosEducativos::INTERMEDIATE
+      else
+        RecursosEducativos::EXPERT
+      end
+    end
   end
 end
