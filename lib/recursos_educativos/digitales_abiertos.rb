f6f6e060d2150826e66520bb13882417d135ed7b
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-15 18:39:30 +0000

    añadida la sobrecarga del operador <=>para la clase digitales abiertos

--- lib/recursos_educativos/digitales_abiertos.rb
+++ lib/recursos_educativos/digitales_abiertos.rb
@@ -1,6 +1,7 @@
 module RecursosEducativos
   class DigitalesAbiertos < Recurso
     attr_reader :uri, :fecha_creacion
+    #include Comparable
     def initialize(id, marca, titulo, descripcion, nivel_experiencia,tipo_actividad, categoria, material, temporalizacion, concepto, uri, fecha_creacion)
           super(id, marca, titulo, descripcion, nivel_experiencia,tipo_actividad, categoria, material, temporalizacion, concepto)
     @uri = uri
@@ -11,8 +12,19 @@ module RecursosEducativos
       "Recurso: Identificacion: #{@id} - Marca: #{@marca} - Titulo: #{@titulo} - Descripcion: #{@descripcion} - Experiencia: #{@nivel_experiencia} - Tipo: #{@tipo_actividad} - Categoria: #{@categoria} - Material: #{@material} - Temporalizacion: #{@temporalizacion} - Conceptos: #{array_string}, - URI: #{@uri}, -Fecha de creacion: #{@fecha_creacion}"
     end
 
-    def <=>(otro)
-      fecha_creacion <=> fecha_creacion
+    def <=>(otro_objeto)
+    # Descomponer las fechas en día, mes y año
+    dia, mes, anio = @fecha_creacion.split("-").map(&:to_i)
+    otro_dia, otro_mes, otro_anio = otro_objeto.fecha_creacion.split("-").map(&:to_i)
+
+    # Comparar año, mes y día en ese orden
+    if anio != otro_anio
+      return anio <=> otro_anio
+    elsif mes != otro_mes
+      return mes <=> otro_mes
+    else
+      return dia <=> otro_dia
     end
+  end
   end
 end
