2c68798392a341e2300dbc7baa9d8873eb381f03
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-21 17:17:14 +0000

    añadido el initialize de clase entorno educacion formal

--- lib/recursos_educativos/entorno_educacion_formal.rb
+++ lib/recursos_educativos/entorno_educacion_formal.rb
@@ -1,4 +1,8 @@
 module RecursosEducativos
   class EntornoEducacionFormal < EntornoDigital
+    def initialize(id_code,nombre,categoria,coleccion = [],numero_niveles)
+      super(id_code,nombre,categoria,coleccion = [])
+      @numero_niveles = numero_niveles
+    end
   end
 end
