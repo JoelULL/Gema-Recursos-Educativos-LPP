3b7e445c86e650326485e1a1d089f458ceefcc5f
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-21 17:17:43 +0000

    comprobada la instancia del objeto con el nuevo atributo

--- test/entorno_educacion_formal_test.rb
+++ test/entorno_educacion_formal_test.rb
@@ -2,8 +2,16 @@ require "test_helper"
 class EntornoEducacionFormalTest < Test::Unit::TestCase
   def test_clase_vacia
     assert_nothing_raised do
-      RecursosEducativos::EntornoEducacionFormal.new(nil,nil,nil,nil)
+      RecursosEducativos::EntornoEducacionFormal.new(nil,nil,nil,nil,nil)
     end
   end
+ # def test_herencia_entorno_digital
+   # assert_instance_of(RecursosEducativos::Entor,@entorno_default)
+   # assert_kind_of(Object,RecursosEducativos::EntornoDigital)
+   # assert_kind_of(Class,RecursosEducativos::EntornoDigital)
+   # assert_kind_of(Module,RecursosEducativos::EntornoDigital)
+   # assert_kind_of(BasicObject,RecursosEducativos::EntornoDigital)
+  #end
+  #
 end
 
