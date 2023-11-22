d0c9b5938eb3798635b3502a01906882316a53b4
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-22 17:16:30 +0000

    añadidas las pruebas para los getters

--- lib/recursos_educativos/entorno_educacion_informal.rb
+++ lib/recursos_educativos/entorno_educacion_informal.rb
@@ -1,10 +1,10 @@
 module RecursosEducativos
   class EntornoEducacionInformal < EntornoDigital
+    attr_reader :precio
     def initialize(id_code,nombre,categoria,coleccion, precio)
       super(id_code,nombre,categoria,coleccion)
       @precio = precio
     end
-
   end
 end
 
