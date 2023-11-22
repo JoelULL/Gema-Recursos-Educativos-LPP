fe41e7b14cfe1651e1bbad95273c2da4124a24bd
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-22 17:08:39 +0000

    realizadas las pruebas del objeto con el nuevo atributo

--- test/entorno_educacion_informal_test.rb
+++ test/entorno_educacion_informal_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 class EntornoEducacionInformalTest < Test::Unit::TestCase
   def test_clase_vacia
     assert_nothing_raised do
-      RecursosEducativos::EntornoEducacionInformal.new(nil,nil,nil,nil)
+      RecursosEducativos::EntornoEducacionInformal.new(nil,nil,nil,nil,nil)
     end
   end
 end
