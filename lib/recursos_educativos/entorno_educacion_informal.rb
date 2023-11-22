03b173f4ba080468014525b0c117abb8c0ec5d6b
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-22 17:08:19 +0000

    añadido el atributo precio a la clase entorno educacion informal

--- lib/recursos_educativos/entorno_educacion_informal.rb
+++ lib/recursos_educativos/entorno_educacion_informal.rb
@@ -1,5 +1,10 @@
 module RecursosEducativos
   class EntornoEducacionInformal < EntornoDigital
+    def initialize(id_code,nombre,categoria,coleccion, precio)
+      super(id_code,nombre,categoria,coleccion)
+      @precio = precio
+    end
+
   end
 end
 
