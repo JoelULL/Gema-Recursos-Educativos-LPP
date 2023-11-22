9d0ae2e626ebb8b673b3401c6b973a832bb3fcb4
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-22 17:25:41 +0000

    añadido el metodo to_s

--- lib/recursos_educativos/entorno_educacion_informal.rb
+++ lib/recursos_educativos/entorno_educacion_informal.rb
@@ -5,6 +5,13 @@ module RecursosEducativos
       super(id_code,nombre,categoria,coleccion)
       @precio = precio
     end
+    def to_s
+      nombres_recursos = []
+      for recurso in coleccion
+        nombres_recursos << recurso.titulo
+      end
+      "- Id :#{@id_code} - nombre: #{@nombre} -categoria: #{@categoria} -coleccion:#{nombres_recursos.join(', ')},#{@precio}"
+    end
   end
 end
 
