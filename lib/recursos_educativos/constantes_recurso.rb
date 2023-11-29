20112ea9ca251915f155d378ea94bc04c26d2485
Joel Dorta <alu0100987584@ull.edu.es>
2023-11-29 18:41:09 +0000

    añadidas las constantes para los criterios a tener en cuenta en el hash del lori

--- lib/recursos_educativos/constantes_recurso.rb
+++ lib/recursos_educativos/constantes_recurso.rb
@@ -3,4 +3,13 @@ module RecursosEducativos
   BEGINNER = :beginner
   INTERMEDIATE = :intermediate
   EXPERT = :expert
+
+  CALIDAD_CONTENIDO = :calidad_contenido
+  OBJETIVOS_APRENDIZAJE = :objetivos_aprendizaje
+  MOTIVACION = :motivacion
+  DISEÑO_PRESENTACION = :diseño_presentacion
+  USABILIDAD = :usabilidad
+  ACCESIBILIDAD = :accesibilidad
+  REHUSABILIDAD = :rehusabilidad
+  VALOR_EDUCATIVO = :valor_educativo
 end
