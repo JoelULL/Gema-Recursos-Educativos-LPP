e28baff4c333429015afdc7ec2ee7e6e30bab32a
Joel Dorta <alu0100987584@ull.edu.es>
2023-12-12 18:19:17 +0000

    añadido el initialize de la clase

--- lib/recursos_educativos/dsleducativo.rb
+++ lib/recursos_educativos/dsleducativo.rb
@@ -1,5 +1,11 @@
 module RecursosEducativos
   class DslEducativo
-
+    def initialize(nombre, &bloque)
+      @nombre = nombre
+      @registros = []
+      @evaluaciones = []
+      @itinerarios = []
+      instance_eval &bloque
+    end
   end
 end
