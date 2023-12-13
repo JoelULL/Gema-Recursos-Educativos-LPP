23560d5b8c9601ecf9e7083d2bcdd3c8a6ae33b1
Joel Dorta <alu0100987584@ull.edu.es>
2023-12-13 19:36:59 +0000

    prueba de la funcion para registrar un recurso

--- test/dsleducativo_test.rb
+++ test/dsleducativo_test.rb
@@ -15,6 +15,17 @@ class DslEducativoTest < Test::Unit::TestCase
      end
     end
   end
+  def setup
+    @d1 = RecursosEducativos::DigitalesAbiertos.new( 015, 'ull', "exploradores de secuencias", 'secuencias', RecursosEducativos::BEGINNER,:guiada, :desenchufada, 'papel', 30, [:algoritmos], 'riull',2023)
+    @d2 = RecursosEducativos::DigitalesAbiertos.new( 003, 'ull', "kidsort", 'bucles', RecursosEducativos::BEGINNER,:guiada, :desenchufada, 'papel', 30, [:algoritmos], 'riull',2023)
+  end
+  def test_registrar
+    dsl_default = RecursosEducativos::DslEducativo.new(001,'vestibulo',:taller)do
+      registrar 015, 'ull', "exploradores de secuencias", 'secuencias', RecursosEducativos::BEGINNER,:guiada, :desenchufada, 'papel', 30, [:algoritmos], 'riull',2023
+      registrar 003, 'ull', "kidsort", 'bucles', RecursosEducativos::BEGINNER,:guiada, :desenchufada, 'papel', 30, [:algoritmos], 'riull',2023
+    end
+    assert_equal([@d1,@d2], dsl_default.recursos) 
+  end
 end
 
 
