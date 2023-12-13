3790262179d2ad016dca6b067fdfc5f0cf2fe8fa
Joel Dorta <alu0100987584@ull.edu.es>
2023-12-13 18:39:17 +0000

    modificado initialize

--- lib/recursos_educativos/dsleducativo.rb
+++ lib/recursos_educativos/dsleducativo.rb
@@ -1,14 +1,22 @@
 module RecursosEducativos
   class DslEducativo
-    def initialize(nombre, &bloque)
-      @nombre = nombre
-      @registros = []
+    def initialize(id_entorno,nombre_entorno,categoria_entorno, &bloque)
+      @recursos = []
+      @entorno = EntornoDigital.new(id_entorno, nombre_entorno,categoria_entorno, [])
       @evaluaciones = []
       @itinerarios = []
       instance_eval &bloque
     end
-    #def registrar_recurso()
+    #def registrar(id,marca,titulo,descripcion,experiencia,tipo,categoria,material,temporalizacion,conceptos,uri?,fecha_creacion?)
+    #if (uri != nil && fecha_creacion != nil)
+     # recurso_nuevo = DigitalesAbiertos.new(id,marca,titulo,descripcion,experiencia,tipo,categoria,material,temporalizacion,conceptos,uri,fecha_creacion)
+      #@recursos << recurso_nuevo
+      #@entorno.coleccion << recurso_nuevo 
+    #else
+     # recurso_nuevo = Recurso.new(id,marca,titulo,descripcion,experiencia,tipo,categoria,material,temporalizacion,conceptos)
+     # @recursos << recurso_nuevo
     #end
+  #end
     #
     #def evaluar_recurso(options = {})
     #end
@@ -20,3 +28,11 @@ module RecursosEducativos
     #end
   end
 end
+                       #id   #nombre   #categoria
+#dsl = DslEducativo.new(001,'vestibulo',:taller) do
+  #registrar 015, 'ull', 'Exploradores...', 'Secuencias',Recursos::INICIAL, :guiada, :desenchufada, 'papel', 30, [:algoritmos], riull, 2023
+  #registrar ...
+  #registrar...
+  #resto de codigo
+#end
+  
